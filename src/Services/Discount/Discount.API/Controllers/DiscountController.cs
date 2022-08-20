97391c5bd617080c7a2e2e96d865833be17b82da
phong.bnh <phong.bnh@globalfactories.com>
2022-08-20 18:53:40 +0700

    feat(discount): add discount controller

---
+++ src/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,50 @@
+using System.Net;
+using Discount.API.Entities;
+using Discount.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Discount.API.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class DiscountController : ControllerBase
+{
+    private readonly IDiscountRepository _repository;
+    private readonly ILogger<DiscountController> _logger;
+
+    public DiscountController(IDiscountRepository repository, ILogger<DiscountController> logger)
+    {
+        _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    [HttpGet("{productName}", Name = "GetDiscount")]
+    [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult<Coupon>> GetDiscount(string productName)
+    {
+        var coupon = await _repository.GetDiscount(productName);
+        return Ok(coupon);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult<Coupon>> CreateDiscount([FromBody] Coupon coupon)
+    {
+        await _repository.CreateDiscount(coupon);
+        return CreatedAtRoute("GetDiscount", new { productName = coupon.ProductName }, coupon);
+    }
+
+    [HttpPut]
+    [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult<Coupon>> UpdateDiscount([FromBody] Coupon coupon)
+    {
+        return Ok(await _repository.UpdateDiscount(coupon));
+    }
+
+    [HttpDelete("{productName}", Name = "DeleteDiscount")]
+    [ProducesResponseType(typeof(bool), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult<bool>> DeleteDiscount(string productName)
+    {
+        return Ok(await _repository.DeleteDiscount(productName));
+    }
+}
