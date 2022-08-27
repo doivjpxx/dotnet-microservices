b527975360ef0709710470f62375036831116eaf
phong.bnh <phong.bnh@globalfactories.com>
2022-08-27 20:29:38 +0700

    feat(order): add update order command

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
@@ -0,0 +1,40 @@
+using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistences;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Features.Orders.Commands.UpdateOrder;
+
+public class UpdateOrderCommandHandler : IRequestHandler<UpdateOrderCommand>
+{
+    private IOrderRepository _orderRepository;
+    private IMapper _mapper;
+    private ILogger<UpdateOrderCommandHandler> _logger;
+
+    public UpdateOrderCommandHandler(IOrderRepository repository, IMapper mapper,
+        ILogger<UpdateOrderCommandHandler> logger)
+    {
+        _orderRepository = repository;
+        _mapper = mapper;
+        _logger = logger;
+    }
+
+    public async Task<Unit> Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
+    {
+        var existOrder = await _orderRepository.GetByIdAsync(request.Id);
+
+        if (existOrder == null)
+        {
+            _logger.LogError("Order is not exists");
+        }
+
+        _mapper.Map(request, existOrder, typeof(UpdateOrderCommand), typeof(Order));
+
+        await _orderRepository.UpdateAsync(existOrder);
+
+        _logger.LogInformation($"Order {existOrder.Id} is updated successfully!");
+        
+        return Unit.Value;
+    }
+}
