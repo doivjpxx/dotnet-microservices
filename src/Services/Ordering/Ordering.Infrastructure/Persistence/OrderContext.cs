6c9046b9785ba95578099bdac6ea0f9f83c02cc3
phong.bnh <phong.bnh@globalfactories.com>
2022-09-05 15:12:46 +0700

    feat(order): infra: add repository

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Ordering.Domain.Commons;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Infrastructure.Persistence;
+
+public class OrderContext : DbContext
+{
+    public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<Order> Orders { get; set; }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        foreach (var entry in ChangeTracker.Entries<EntityBase>())
+        {
+            switch (entry.State)
+            {
+                case EntityState.Added:
+                    entry.Entity.CreatedDate = DateTime.Now;
+                    entry.Entity.CreatedBy = "phong";
+                    break;
+                case EntityState.Modified:
+                    entry.Entity.LastModifiedBy = "phong";
+                    entry.Entity.LastModifiedDate = DateTime.Now;
+                    break;
+            }
+        }
+        
+        return base.SaveChangesAsync(cancellationToken);
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Contracts.Persistences;
+using Ordering.Domain.Entities;
+using Ordering.Infrastructure.Persistence;
+
+namespace Ordering.Infrastructure.Repositories;
+
+public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+{
+    public OrderRepository(OrderContext dbContext) : base(dbContext)
+    {
+    }
+
+    public async Task<IEnumerable<Order>> GetOrderByUserName(string userName)
+    {
+        var orderList = await _dbContext.Orders.Where(x => x.UserName == userName).ToListAsync();
+        return orderList;
+    }
+}
