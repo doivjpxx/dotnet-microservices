e850cea4126071431fff51c3dfb14c3bba9c4c77
phong.bnh <phong.bnh@globalfactories.com>
2023-09-06 14:51:55 +0700

    feat: init ocelot gateway

---
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -0,0 +1,30 @@
+using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+builder.Services.AddOcelot().AddCacheManager(settings => settings.WithDictionaryHandle());
+
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
+}).UseSerilog();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello world!"); });
+});
+
+await app.UseOcelot();
+
+app.Run();
